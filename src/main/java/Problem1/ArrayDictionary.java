842b329090e68a7a656f8c4f9a817caeeebe1e00
zhamid01 <zhamid01@uw.edu>
2020-12-11 22:14:57 -0800

    working on remove function

--- src/main/java/Problem1/ArrayDictionary.java
+++ src/main/java/Problem1/ArrayDictionary.java
@@ -45,7 +45,22 @@ public class ArrayDictionary implements Dictionary {
 
     @Override
     public void remove(String key) {
-        // homework
+        if (key == null) {
+            return;
+        }
+        int d1 = hashFunction(key);
+        KVEntry n1 = null;
+        KVEntry n2 = entries[d1];
+        while (n2.next != null && n2.value != key) {
+            n1 = n2;
+            n2 = n2.next;
+        }
+        if (n2.value == key) {
+            if (n1 != null) {
+                n1.next = n2;
+            }
+            entries[d1] = n2.next;
+        }
     }
 
     @Override
